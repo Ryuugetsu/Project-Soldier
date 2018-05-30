ecab15836d54577218f807b3ff8f66e0bfae635a
Ryuugetsu <38413543+Ryuugetsu@users.noreply.github.com>
2018-05-30 09:13:26 -0300

    menu e animações incompleto

---
+++ Project Soldier/Assets/Main Menu/Script/MainMenu.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour {
+
+    public AudioMixer _audioMixer;
+    public Dropdown _resolutionDropdown;
+
+    private Resolution[] _resolutions;
+
+
+    private void Start()
+    {
+        GetUnityResolutions();
+    }
+
+    public void PlayGame()
+    {
+        //envia para a proxima scene de acordo com a ordem na lista de build
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        //fecha o jogo
+        Application.Quit();
+    }
+
+    public void SetVolume(float volume)
+    {
+        //alteração do volume do jogo
+        _audioMixer.SetFloat("Volume", volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        //troca entre os niveis de qualidades
+        QualitySettings.SetQualityLevel(qualityIndex);        
+    }
+
+    private void GetUnityResolutions()
+    {
+        //pegar todas as resoluções disponiveis
+        _resolutions = Screen.resolutions;
+        //limpar as opções atuais do dropdown
+        _resolutionDropdown.ClearOptions();
+
+        //criar lista para popular o dropdown
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        //varrer o vetor com as resoluções e popular lista com as mesmas
+        for (int i = 0; i < _resolutions.Length; i++)
+        {
+            string option = _resolutions[i].width + "X" + _resolutions[i].height;
+            options.Add(option);
+
+            //armazena a atual posição da lista que equivale a resolução do monitos do usuario
+            if (_resolutions[i].width == Screen.currentResolution.width && _resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+        //popular dropdown com as resoluções já armazenadas na lista options
+        _resolutionDropdown.AddOptions(options);
+        //seta a resolução do monitor como a padrão do dropdown
+        _resolutionDropdown.value = currentResolutionIndex;
+        _resolutionDropdown.RefreshShownValue();
+    }
+
+    public void SetResolutions(int resolutionIndex)
+    {
+        //armazena a resolução selecionada do vetor com todas as resoluções com base no index recebido
+        Resolution resolution = _resolutions[resolutionIndex];
+        //seta a resolução e o full screen
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        //full screen
+        Screen.fullScreen = isFullscreen;
+    }
+
+}
